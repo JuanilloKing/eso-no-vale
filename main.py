6b263218af6b2ff4a4f26e2f14c07a6b702ee7be
Alejandro Rios <alejandro.rios.cyt@gmail.com>
2025-12-05 17:43:44 +0100

    Corrección import y nomenclatura

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ print("[1] Hundir la Flota")
 print("[2] Buscaminas")
 
 opcion = input("Selecciona una opción (1 o 2): ")
+
 if opcion == "1":
-    import hundir_la_flota
-    hundir_la_flota.jugar()
+    # Import desde subcarpeta hundir-la-flota
+    from hundir_la_flota.hundirLaFlota_main import jugar
+    jugar()
+
 elif opcion == "2":
-    import buscamina
-    buscamina.jugar()
+    # Import desde subcarpeta buscaminas
+    from buscaminas.buscaminas_main import jugar
+    jugar()
+
 else:
     print("Opción no válida. Por favor, selecciona 1 o 2.")
