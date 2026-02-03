5abc2b1f73ba0123cc4c7d61ef168c77848cfa44
casludx <lucasfloresben@gmail.com>
2026-02-03 11:04:58 +0100

    Mejora en main.py

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 def play():
     print("Bienvenido al menú!")
-    print("¿Qué juego quieres jugar?")
-    print("[1] Hundir la Flota")
-    print("[2] Buscaminas")
+    
+    while True: 
+        print("¿Qué juego quieres jugar?")
+        print("[1] Hundir la Flota")
+        print("[2] Buscaminas")
 
-    opcion = input("Selecciona una opción (1 o 2): ")
+        opcion = input("Selecciona una opción (1 o 2): ")
 
-    if opcion == "1":
-        # Import desde subcarpeta hundir-la-flota
-        from hundir_la_flota.hundirLaFlota_main import jugar
-        jugar()
+        if opcion == "1":
+            # Import desde subcarpeta hundir-la-flota
+            from hundir_la_flota.hundirLaFlota_main import jugar
+            jugar()
+            break
 
-    elif opcion == "2":
-        # Import desde subcarpeta buscaminas
-        from buscaminas.buscaminas_main import jugar
-        jugar()
-
-    else:
-        print("Opción no válida. Por favor, selecciona 1 o 2.")
-play()
+        elif opcion == "2":
+            # Import desde subcarpeta buscaminas
+            from buscaminas.buscaminas_main import jugar
+            jugar()
+            break
+        
+        else:
+            print("Opción no válida. Por favor, selecciona 1 o 2.")
+            print()
+play()  
